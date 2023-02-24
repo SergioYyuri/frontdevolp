172b200c8696dd81b52bacf72c915f12f88a1045
SergioYyuri <105612136+SergioYyuri@users.noreply.github.com>
2023-02-23 22:15:06 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function Enviar() { 
+    var nome = document.getElementById("nomeid");
+    var nome = document.getElementById("foneid"); 
+    var nome = document.getElementById("emailid"); 
+    if (nome.value != "") { 
+    alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso'); 
+        }
+    }
+    
